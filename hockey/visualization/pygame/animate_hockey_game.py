fb2783e68b36ae3bad3bef50608cc4998f369b33
Luis Da Costa <dacosta.le@gmail.com>
2017-10-23 17:01:07 -0400

    Animation: possibility of observing just certain frames

--- hockey/visualization/pygame/animate_hockey_game.py
+++ hockey/visualization/pygame/animate_hockey_game.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import pandas as pd
 import time
+import numpy as np
 
 from geometry.point import Point
 from hockey.core.model import TIME_PER_FRAME
@@ -18,21 +19,25 @@ from hockey.visualization.pygame.global_def import HALF_ICE_WIDTH, HALF_ICE_HEIG
 
 if __name__ == "__main__":
     # field
-    hockey_rink = HockeyHalfRink(how_many_offense=5, how_many_defense=5, one_step_in_seconds=TIME_PER_FRAME, collect_data_every_secs=1.5)
+    DATA_EVERY_SECS = TIME_PER_FRAME # .1 # 1 / 30
+    RECORD_THIS_MANY_MINUTES = 60
+    hockey_rink = HockeyHalfRink(how_many_offense=1, how_many_defense=0, one_step_in_seconds=TIME_PER_FRAME, collect_data_every_secs=DATA_EVERY_SECS, record_this_many_minutes=RECORD_THIS_MANY_MINUTES)
 
-    if False:
+    run_simulation = True
+    if run_simulation:
         mesa_simulator = MesaModelSimulator(mesa_model=hockey_rink)
         hockey_problem = LearnToPlayHockeyProblem(hockey_world=hockey_rink)
         xcs_simulator = ScenarioSimulator(xcs_scenario=hockey_problem)
-        mesa_simulator.run()
-
-        print("allo")
+        # mesa_simulator.run()
+        xcs_simulator.run()
 
         hockey_rink.datacollector.get_model_vars_dataframe().to_csv("/tmp/model.pd")
         hockey_rink.datacollector.get_agent_vars_dataframe().to_csv("/tmp/agents.pd")
 
     model_df = pd.read_csv("/tmp/model.pd")
     num_steps = model_df["Unnamed: 0"].max()
+    num_goals = model_df["goals"].max()
+    print("[Recording length: %d minutes] %d steps of simulation, %d goals" % (RECORD_THIS_MANY_MINUTES, num_steps, num_goals))
     agents_df = pd.read_csv("/tmp/agents.pd")
 
     pygame.init()
@@ -46,14 +51,29 @@ if __name__ == "__main__":
 
     FPS = 30
 
-    for i in range(num_steps + 1):
+    DISPLAY_EVERY_SECS = .5
+    steps_between_displays = round(DISPLAY_EVERY_SECS / DATA_EVERY_SECS)
+    SPEED_FACTOR = -1 # -1 means: go as fast as possible!
+    if SPEED_FACTOR > 0:
+        total_time_between_frames = (DISPLAY_EVERY_SECS * 1000) / SPEED_FACTOR
+    else:
+        total_time_between_frames = 0
+
+    times_between_frames = []
+    report_every_frames = 10
+    report_every_this_many_secs = 5
+
+
+
+
+    for i in range(0, num_steps + 1, steps_between_displays):
         df_step = agents_df.loc[agents_df['Step'] == i]
-        print("Step %d/%d" % (i, num_steps))
+        # print("Step %d/%d" % (i, num_steps))
         defenses_seen = 0
         attackers_seen = 0
         for idx, row in df_step.iterrows():
             agent_id = row['AgentID']
-            print("              " + agent_id)
+            # TODO: update speed too!
             the_pos = Point(x=row['pos_x'], y=row['pos_y'])
             if agent_id.startswith("defense"):
                 hockey_rink.defense[defenses_seen].pos = the_pos
@@ -65,14 +85,24 @@ if __name__ == "__main__":
                 hockey_rink.puck.pos = the_pos
             else:
                 assert False
-        tick_time = clock.tick(FPS)
+        tick_time = clock.tick() # (FPS)
+        times_between_frames.append(tick_time)
+        # print("this many: %.2f milliseconds since last update" % (tick_time))
         surface.fill(THECOLORS['black']) # this is basically a CLEAR. TODO: can we do something better????
+        pygame.event.get()
 
         pygame_render(hockey_rink_renderable.representation(), surface)
         pygame.display.update()
-        time.sleep(5)
-
-
+        if SPEED_FACTOR > 0:
+            sleep_this_much = total_time_between_frames - tick_time
+            if sleep_this_much > 0:
+                print("Sleeping %.2f ms., as %.2f of %.2f have already been elapsed" % (sleep_this_much, tick_time, total_time_between_frames))
+                time.sleep(sleep_this_much)
+        if len(times_between_frames) == report_every_frames:
+            time_in_ms = np.mean(times_between_frames)
+            print("Avg time (last %d frames, ~ %d seconds) between frames = %.2f ms." % (report_every_frames, report_every_this_many_secs, time_in_ms))
+            report_every_frames = (1000 // time_in_ms) * report_every_this_many_secs # so I will report approx. once per second
+            times_between_frames = []
 
     if False:
 
