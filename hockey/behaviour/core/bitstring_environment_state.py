e4bd2dce096228db38e63edeeb102ddd3ce09ac7
Luis Da Costa <dacosta.le@gmail.com>
2017-10-31 11:08:20 -0400

    bitstring representation: do a one-hot-encoding-ish for distances (to puck, goal)

--- hockey/behaviour/core/bitstring_environment_state.py
+++ hockey/behaviour/core/bitstring_environment_state.py
@@ -19,20 +19,25 @@ class BitstringEnvironmentState(object):
         # (1) to puck (if I can see puck only! Otherwise I can't evaluate the distance!)
         dist_to_puck = self.full_state.distance_to_puck()
         as_list.append(int(dist_to_puck <= 10) * can_see_puck_as_int)
-        as_list.append(int(dist_to_puck <= 20) * can_see_puck_as_int)
-        as_list.append(int(dist_to_puck <= 30) * can_see_puck_as_int)
-        as_list.append(int(dist_to_puck <= 40) * can_see_puck_as_int)
-        as_list.append(int(dist_to_puck <= 50) * can_see_puck_as_int)
-        as_list.append(int(dist_to_puck <= 60) * can_see_puck_as_int)
+        as_list.append(int((dist_to_puck > 10) and (dist_to_puck <= 20)) * can_see_puck_as_int)
+        as_list.append(int((dist_to_puck > 20) and (dist_to_puck <= 30)) * can_see_puck_as_int)
+        as_list.append(int((dist_to_puck > 30) and (dist_to_puck <= 40)) * can_see_puck_as_int)
+        as_list.append(int((dist_to_puck > 40) and (dist_to_puck <= 50)) * can_see_puck_as_int)
+        as_list.append(int((dist_to_puck > 50) and (dist_to_puck <= 60)) * can_see_puck_as_int)
+        as_list.append(int(dist_to_puck > 60) * can_see_puck_as_int)
         # (2) to goal
+        # can I see the goal?
+        can_see_goal_as_int = int(self.full_state.me.can_see_goal())
+        as_list.append(can_see_goal_as_int)
         dist_to_goal = self.full_state.distance_to_goal()
-        as_list.append(int(dist_to_goal <= 10))
-        as_list.append(int(dist_to_goal <= 20))
-        as_list.append(int(dist_to_goal <= 30))
-        as_list.append(int(dist_to_goal <= 40))
-        as_list.append(int(dist_to_goal <= 50))
-        as_list.append(int(dist_to_goal <= 60))
-
+        as_list.append(int(dist_to_goal <= 10) * can_see_puck_as_int)
+        as_list.append(int((dist_to_goal > 10) and (dist_to_goal <= 20)) * can_see_goal_as_int)
+        as_list.append(int((dist_to_goal > 20) and (dist_to_goal <= 30)) * can_see_goal_as_int)
+        as_list.append(int((dist_to_goal > 30) and (dist_to_goal <= 40)) * can_see_goal_as_int)
+        as_list.append(int((dist_to_goal > 40) and (dist_to_goal <= 50)) * can_see_goal_as_int)
+        as_list.append(int((dist_to_goal > 50) and (dist_to_goal <= 60)) * can_see_goal_as_int)
+        as_list.append(int(dist_to_goal > 60) * can_see_goal_as_int)
+        # ok then! Return!
         return XCSBitString(as_list)
 
 
