40b0d943333a00ce8a0ebad8e97bc6b8bedb4d2d
Luis Da Costa <dacosta.le@gmail.com>
2017-11-02 14:38:04 -0400

    Provide options for saving and retrieving model

--- hockey/core/simulator.py
+++ hockey/core/simulator.py
@@ -1,16 +1,18 @@
 
 
 import abc
-
+import pickle
 from mesa import Model
+from pathlib import Path
+import os
 
+from typing import Optional
 import xcs
 from xcs.scenarios import Scenario
 from xcs import XCSAlgorithm
 import logging
 from xcs import XCSAlgorithm
 from xcs.scenarios import MUXProblem, ScenarioObserver
-
 
 class Simulator(metaclass=abc.ABCMeta):
 
@@ -42,10 +44,21 @@ class MesaModelSimulator(Simulator):
 
 class ScenarioSimulator(Simulator):
 
-    def __init__(self, xcs_scenario: Scenario):
+    MODELS_DIR = "/tmp/luis/models" # TODO: change!!!!!
+    os.makedirs(MODELS_DIR, exist_ok=True)
+
+    def __init__(self, xcs_scenario: Scenario, load_from_file_name: Optional[str], save_to_file_name: Optional[str]):
         Simulator.__init__(self)
         self.scenario = ScenarioObserver(xcs_scenario)
         self.running = False
+        if load_from_file_name is None:
+            self.load_from = None
+        else:
+            self.load_from = os.path.join(ScenarioSimulator.MODELS_DIR, load_from_file_name)
+        if save_to_file_name is None:
+            self.save_to = None
+        else:
+            self.save_to = os.path.join(ScenarioSimulator.MODELS_DIR, save_to_file_name)
 
     def run(self):
         self.running = True
@@ -65,19 +78,32 @@ class ScenarioSimulator(Simulator):
         # algorithm.mutation_probability = .002
 
 
+
+        # Use the built-in pickle module to save/reload your model for reuse.
+        # import pickle
+        # pickle.dump(model, open('model.bin', 'wb'))
+        # reloaded_model = pickle.load(open('model.bin', 'rb'))
+
         # scenario = MUXProblem()
         # algorithm = XCSAlgorithm()
         # algorithm.exploration_probability = .1
-        model = algorithm.run(self.scenario)
-        #
-        #
-        # # Create a classifier set from the algorithm, tailored for the
-        # # scenario you have selected.
-        # model = algorithm.new_model(self.scenario)
-        #
-        # # Run the classifier set in the scenario, optimizing it as the
-        # # scenario unfolds.
-        # model.run(self.scenario, learn=True)
+        # model = algorithm.run(self.scenario)
+
+        if (self.load_from is not None) and Path(self.load_from).is_file():
+            print("Loading model from file '%s'..." % (self.load_from))
+            model = pickle.load(open(self.load_from, 'rb'))
+        else:
+            if self.load_from is not None:
+                print("File '%s' does not exist." % (self.load_from))
+            # Create a classifier set from the algorithm, tailored for the
+            # scenario you have selected.
+            print("Creating new algorithm for scenario...")
+            model = algorithm.new_model(self.scenario)
+        print("Done")
+
+        # Run the classifier set in the scenario, optimizing it as the
+        # scenario unfolds.
+        model.run(self.scenario, learn=True)
 
         # Get a quick list of the best classifiers discovered.
         good_rules = 0
@@ -91,6 +117,11 @@ class ScenarioSimulator(Simulator):
                     print(rule.condition, '=>', rule.action, ' [%.5f]' % rule.fitness)
         if good_rules < 5:
             print("Only %d 'good' rules found" % good_rules)
+
+        if (self.save_to is not None):
+            print("Saving model into file '%s'..." % (self.save_to))
+            pickle.dump(model, open(self.save_to, 'wb'))
+            print("Done")
 
         # steps, reward, seconds, model = xcs.test(algorithm, scenario=self.scenario) # algorithm=XCSAlgorithm,
         self.running = False
