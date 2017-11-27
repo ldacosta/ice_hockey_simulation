1708b2216eeb27b2db9b9eb8e9382a35c524d964
Luis Da Costa <dacosta.le@gmail.com>
2017-11-27 10:04:45 -0500

    Changes of options when running

--- hockey/core/simulator.py
+++ hockey/core/simulator.py
@@ -6,14 +6,21 @@ from mesa import Model
 from pathlib import Path
 import os
 import time
+import glob
 
 from hockey.behaviour.core.bitstring_environment_state import BitstringEnvironmentState
 from hockey.behaviour.core.hockey_scenario import LearnToPlayHockeyProblem
-from typing import Optional
+from typing import Optional, Tuple
 import xcs
 import logging
 from xcs.scenarios import ScenarioObserver
 import xcs.bitstrings
+
+import numpy as np
+from geometry.point import Point
+from geometry.vector import Vec2d
+from hockey.behaviour.core.action import HockeyAction
+
 
 
 class Simulator(metaclass=abc.ABCMeta):
@@ -49,18 +56,27 @@ class ScenarioSimulator(Simulator):
     MODELS_DIR = "/tmp/luis/models" # TODO: change!!!!!
     os.makedirs(MODELS_DIR, exist_ok=True)
 
-    def __init__(self, xcs_scenario: LearnToPlayHockeyProblem, load_from_file_name: Optional[str], save_to_file_name: Optional[str]):
+    def __init__(self,
+                 xcs_scenario: LearnToPlayHockeyProblem,
+                 load_from_dir_name: Optional[str],
+                 load_from_file_name: Optional[str],
+                 save_to_dir_name: Optional[str]):
         Simulator.__init__(self)
         self.hockey_problem = xcs_scenario
         self.running = False
-        if load_from_file_name is None:
-            self.load_from = None
-        else:
-            self.load_from = os.path.join(ScenarioSimulator.MODELS_DIR, load_from_file_name)
-        if save_to_file_name is None:
-            self.save_to = None
-        else:
-            self.save_to = os.path.join(ScenarioSimulator.MODELS_DIR, save_to_file_name)
+        if load_from_dir_name is not None:
+            if not Path(load_from_dir_name).is_dir():
+                raise RuntimeError("[loading brain] '%s' is not a directory" % (load_from_dir_name))
+        self.load_from_dir_name = load_from_dir_name
+        self.load_from_file_name = load_from_file_name
+        if load_from_dir_name is not None and load_from_file_name is not None:
+            load_from = os.path.join(self.load_from_dir_name, self.load_from_file_name)
+            if not Path(load_from).is_file():
+                raise RuntimeError("[loading brain] File '%s' does not contain a brain" % (load_from))
+        if save_to_dir_name is not None:
+            if not Path(save_to_dir_name).is_dir():
+                raise RuntimeError("[saving brain] '%s' is not a directory" % (save_to_dir_name))
+        self.save_to_dir_name = save_to_dir_name
 
     def run_until_done(self):
         start_time = time.time()
@@ -71,33 +87,28 @@ class ScenarioSimulator(Simulator):
             print("run_until_done -> time so far: %.2f secs. (minute %d)" % (elapsed_time, int(elapsed_time // 60)))
         print("run_until_done -> DONE")
 
+    def __find_newest_brain_in_dir(self, directory: str) -> Optional[str]:
+        full_path_wildcard = os.path.join(directory, '*.bin')
+        try:
+            return max(glob.iglob(full_path_wildcard), key=os.path.getctime)
+        except Exception:
+            return None
+
     def run(self):
         self.scenario = ScenarioObserver(self.hockey_problem)
         def show_good_rules(model):
             dict = {}
-            dict.get("lalala", "default")
             for rule in model:
                 dict[rule.action] = dict.get(rule.action, []) + [rule]
-            print("Actions: %s" %  (dict.keys()))
+            # print("Actions: %s" %  (dict.keys()))
             dict.update((k, sorted(rule_list, key=lambda rule: rule.fitness, reverse=True)) for k, rule_list in dict.items())
-            print("Best rules: ")
+            print("Best rules: (only showing actions that have rules with fitness > 0.5")
             for act, rule_list in dict.items():
-                print("%s: %s" % (act, rule_list[0]))
-
-
-            good_rules = 0
-            for rule in model:
-                if rule.fitness > .5:
-                    good_rules += 1
-                    print(rule.condition, '=>', rule.action, ' [%.5f, experience: %d]' % (rule.fitness, rule.experience))
-                    # if rule.fitness > .75:
-                    #     print("********************************************************************")
-                    #     print(rule.condition, '=>', rule.action,
-                    #           ' [%.5f, experience: %d]' % (rule.fitness, rule.experience))
-                    #     print(BitstringEnvironmentState.explain_condition(condition=rule.condition))
-
-            if good_rules < 5:
-                print("Only %d 'good' rules found" % good_rules)
+                good_rules = [r for r in rule_list if r.fitness > 0.5]
+                if len(good_rules) > 0:
+                    print("Action %s: %d good rules: " % (act, len(good_rules)))
+                    for rule in good_rules:
+                        print("\t %s (fitness: %.2f, experience: %d, avg. reward: %.2f, error: %.2f)" % (rule.condition, rule.fitness, rule.experience, rule.average_reward, rule.error))
 
         self.running = True
 
@@ -127,24 +138,34 @@ class ScenarioSimulator(Simulator):
         # algorithm.exploration_probability = .1
         # model = algorithm.run(self.scenario)
 
-        if (self.load_from is not None) and Path(self.load_from).is_file():
-            last_modified_str = time.ctime(os.stat(self.load_from).st_mtime)
-            print("Loading model from file '%s' (last modified on %s)..." % (self.load_from, last_modified_str))
-            model = pickle.load(open(self.load_from, 'rb'))
-            show_good_rules(model)
+
+        create_new = False
+        if self.load_from_dir_name is None:
+            create_new = True
+        elif self.load_from_file_name is None:
+            load_from = self.__find_newest_brain_in_dir(self.load_from_dir_name)
+            if load_from is None:
+                create_new = True
         else:
-            if self.load_from is not None:
-                print("File '%s' does not exist." % (self.load_from))
+            load_from = os.path.join(self.load_from_dir_name, self.load_from_file_name)
+            if not Path(load_from).is_file():
+                raise RuntimeError("File '%s' does not contain a brain" % (load_from))
+        if create_new:
             # Create a classifier set from the algorithm, tailored for the
             # scenario you have selected.
             print("Creating new algorithm for scenario...")
             model = algorithm.new_model(self.scenario)
+        else:
+            last_modified_str = time.ctime(os.stat(load_from).st_mtime)
+            print("Loading model from file '%s' (last modified on %s)..." % (load_from, last_modified_str))
+            model = pickle.load(open(load_from, 'rb'))
+            show_good_rules(model)
         print("Loading/Creation Done")
 
         model.algorithm.exploration_probability = .25
-        model.algorithm.crossover_probability = .25
-        model.algorithm.mutation_probability = .02
-        model.algorithm.discount_factor = 0.95 # 0.02 #
+        # model.algorithm.crossover_probability = .25
+        # model.algorithm.mutation_probability = .02
+        # model.algorithm.discount_factor = 0.95 # 0.02 #
 
         # print(model.algorithm.discount_factor)
         # input("Press Enter to start simulation...")
@@ -155,16 +176,30 @@ class ScenarioSimulator(Simulator):
         # Get a quick list of the best classifiers discovered.
         # show_good_rules(model)
 
-        if (self.save_to is not None):
-            print("Saving model into file '%s'..." % (self.save_to))
-            pickle.dump(model, open(self.save_to, 'wb'))
+        if self.save_to_dir_name is not None:
+            brain_file_name, full_brain_file_name = self.__chose_brain_file_name()
+            print("Saving model into file '%s'..." % (full_brain_file_name))
+            pickle.dump(model, open(full_brain_file_name, 'wb'))
             print("Saving Done")
+            # set up next loading
+            self.load_from_dir_name = self.save_to_dir_name
+            self.load_from_file_name = brain_file_name
 
         # steps, reward, seconds, model = xcs.test(algorithm, scenario=self.scenario) # algorithm=XCSAlgorithm,
         self.running = False
 
+    def __chose_brain_file_name(self) -> Tuple[str, str]:
+        BRAIN_FILENAME_TEMPLATE = "brain_episode_%d.bin"
+        num_brain_file = 1
+        brain_file_name = BRAIN_FILENAME_TEMPLATE % num_brain_file
+        full_brain_file_name = os.path.join(self.save_to_dir_name, brain_file_name)
+        print("[choose brain file name] Trying '%s'..." % (full_brain_file_name))
+        while os.path.exists(full_brain_file_name):
+            num_brain_file += 1
+            brain_file_name = BRAIN_FILENAME_TEMPLATE % num_brain_file
+            full_brain_file_name = os.path.join(self.save_to_dir_name, brain_file_name)
+            print("[choose brain file name] Trying '%s'..." % (full_brain_file_name))
+        return brain_file_name, full_brain_file_name
+
     def is_running(self) -> bool:
         return self.running
-
-
-
