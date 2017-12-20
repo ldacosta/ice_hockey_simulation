ae2b3a5f3703885cb7d1a4c329162225d302828f
Luis Da Costa <dacosta.le@gmail.com>
2017-12-20 16:51:42 -0500

    world2canvas transformation: extension and testing

---
+++ hockey/visualization/pygame/test_world_to_canvas.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""Base class for Rendering on pygame.
+
+TODO:
+
+"""
+
+import unittest
+import math
+from random import randint
+
+from geometry.point import Point
+
+from hockey.visualization.pygame.world_to_canvas import World2CanvasConverter
+
+class TestWorld2Canvas(unittest.TestCase):
+    """Testing transformations of world 2 canvas."""
+
+    def setUp(self):
+        """Initialization"""
+        self.transformer = World2CanvasConverter(world_height=100, world_width=50)
+
+    def test_limits(self):
+        print(self.transformer)
+        # origin
+        world_origin = Point(0, 0)
+        result = self.transformer.world_2_screen(world_origin)
+        self.assertAlmostEqual(result.x, 0)
+        self.assertAlmostEqual(result.y, self.transformer.screen_height)
+        # bottom left
+        world_bottom_left = Point(self.transformer.world_width, self.transformer.world_height)
+        result = self.transformer.world_2_screen(world_bottom_left)
+        self.assertAlmostEqual(result.x, self.transformer.screen_width)
+        self.assertAlmostEqual(result.y, 0)
+
+    def test_belonging_of_transformed(self):
+        "Points inside the world should get transformed to points inside the screen."
+        for _ in range(10):
+            world_x = randint(0, int(math.floor(self.transformer.world_width)))
+            world_y = randint(0, int(math.floor(self.transformer.world_height)))
+            world_pt = Point(world_x, world_y)
+            result = self.transformer.world_2_screen(world_pt)
+            msg = "World pt %s got converted into screen point %s" % (world_pt, result)
+            self.assertTrue(result.x >= 0 and result.x <= self.transformer.screen_width, msg)
+            self.assertTrue(result.y >= 0 and result.y <= self.transformer.screen_height, msg)
+
+    def test_homogeneous_coordinates(self):
+        "Points with same coordinates should stay that way."
+        for _ in range(10):
+            a_value = randint(0, min(int(math.floor(self.transformer.world_width)), int(math.floor(self.transformer.world_width))))
+            world_pt = Point(a_value, a_value)
+            result = self.transformer.world_2_screen(world_pt)
+            msg = "World pt %s got converted into screen point %s" % (world_pt, result)
+            world_orig_on_screen = self.transformer.world_2_screen(Point(0,0))
+            screen_dist_x = abs(result.x - world_orig_on_screen.x)
+            screen_dist_y = abs(result.y - world_orig_on_screen.y)
+            self.assertAlmostEqual(screen_dist_x, screen_dist_y, msg=msg)
--- hockey/visualization/pygame/world_to_canvas.py
+++ hockey/visualization/pygame/world_to_canvas.py
@@ -1,22 +1,82 @@
+# -*- coding: utf-8 -*-
+"""Conversion of wrold 2 canvas.
+
+TODO:
+
+"""
+
+import tkinter as tk
+import numpy as np
+
+from typing import Tuple
+
+from geometry.point import Point
 from util.base import normalize_to
 
 
 class World2CanvasConverter(object):
-
-    MAX_WIDTH = 1280
-    MAX_HEIGHT = 800
+    """
+    Handles conversions from a world to a canvas.
+    Assumes:
+        * canvas has (0,0) on top-left.
+        * world has (0,0) on bottom-right.
+    """
 
     X_MARGIN = 20
     Y_MARGIN = 20
 
+    @classmethod
+    def screen_width_height(cls) -> Tuple[int, int]:
+        "Returns width and height of current screen."
+        root = tk.Tk()
+        return (root.winfo_screenwidth(), root.winfo_screenheight())
+
     def __init__(self,
                  world_width: float, world_height: float):
+        (max_width, max_height) = World2CanvasConverter.screen_width_height()
+        print("[screen detection] calculated width = %d, height = %d" % (max_width, max_height))
         self.world_width = world_width
         self.world_height = world_height
-        self.size_multiplier = int(min([self.MAX_HEIGHT/self.world_height, self.MAX_WIDTH/self.world_width]))
-        self.screen_width = self.world_width * self.size_multiplier
-        self.screen_height = self.world_height * self.size_multiplier
+        # I need to find MAX screen width and height such that
+        # (world_width/world_height) == (screen_width/screen_height)
+        # let's say that I choose screen width as max_width. What is the value of the height?
+        possible_width = max_width
+        possible_height = (self.world_height * possible_width) / self.world_width
+        if possible_height <= max_height:
+            # sold!
+            self.screen_width = possible_width
+            self.screen_height = possible_height
+        else:
+            # let's say that I choose screen height as max_height. What is the value of the width?
+            possible_height = max_height
+            possible_width = (self.world_width * possible_height) / self.world_height
+            if possible_width <= max_width:
+                # sold!
+                self.screen_width = possible_width
+                self.screen_height = possible_height
+            else:
+                raise RuntimeError("what happened here???")
+        # self.size_multiplier = int(min([max_height/self.world_height, max_width/self.world_width]))
+        # self.screen_width = self.world_width * self.size_multiplier
+        # self.screen_height = self.world_height * self.size_multiplier
+        a = self.screen_width / self.world_width
+        b = self.screen_height / self.world_height
+        self.transformation_matrix = np.zeros((3,3))
+        self.transformation_matrix[0][0] = a
+        self.transformation_matrix[1][1] = -b
+        self.transformation_matrix[1][2] = self.screen_height
+        self.transformation_matrix[2][2] = 1
 
+    def world_2_screen(self, pt_world: Point) -> Point:
+        pt = np.ones((3, 1))
+        pt[0] = pt_world.x
+        pt[1] = pt_world.y
+        result = np.dot(self.transformation_matrix, pt)
+        return Point(x=result[0][0], y=result[1][0])
+
+    def __str__(self):
+        return "World:  width = %.2f, height = %.2f\n" % (self.world_width, self.world_height) + \
+               "Screen: width = %.2f, height = %.2f" % (self.screen_width, self.screen_height)
 
     def x_on_screen(self, x_wc: float) -> float:
         return self.X_MARGIN/2 + normalize_to(a_value=x_wc,
